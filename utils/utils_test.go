4ced9d23dc382589e5aed6a5c02d6e603757aa7f
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-04 09:21:54 +0200

    Utils: add some tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,60 +1,9 @@
 package utils
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestReadInputFileContentsAsString(t *testing.T) {
-	contentString, err := ReadInputFileContentsAsString(3042, 42)
-	exp := `this is a test file for utils testing
-this one is for testing loading content as a string
-as well as a string slice
-using
-newlines
-as a
-separator`
-	if err != nil {
-		t.Errorf("error loading file: %s", err)
-	}
-	if contentString != exp {
-		t.Error("content mismatch")
-	}
-}
-
-func TestReadInputFileContentsAsStringSlice(t *testing.T) {
-	contentStringSlice, err := ReadInputFileContentsAsStringSlice(3042, 42, "\n")
-	exp := []string{
-		"this is a test file for utils testing",
-		"this one is for testing loading content as a string",
-		"as well as a string slice",
-		"using",
-		"newlines",
-		"as a",
-		"separator",
-	}
-	if err != nil {
-		t.Errorf("error loading file: %s", err)
-	}
-	for index, value := range exp {
-		if contentStringSlice[index] != value {
-			t.Error("content mismatch")
-		}
-	}
-}
-
-func TestReadInputFileContentsAsIntSlice(t *testing.T) {
-	contentIntSlice, err := ReadInputFileContentsAsIntSlice(3042, 106, ",")
-	exp := []int{123,55,66,7,8,9,2024}
-	if err != nil {
-		t.Errorf("error loading file: %s", err)
-	}
-	for index, value := range exp {
-		if contentIntSlice[index] != value {
-			t.Error(fmt.Sprintf("content mismatch, want %d, but got %d", value, contentIntSlice[index]))
-		}
-	}
-}
 
 func TestSumIntSlice(t *testing.T) {
 	tcs := []struct {
@@ -82,5 +31,32 @@ func TestSumIntSlice(t *testing.T) {
 		if res := SumIntSlice(tc.testInput); res != tc.expectedOutput {
 			t.Errorf("incorrect int slice sum, expected %d, but got %d", tc.expectedOutput, res)
 		}
+	}
+}
+
+func TestSlicesIntEqual(t *testing.T) {
+	a := []int{123, 44, 55, -29, 42}
+	b := []int{123, 44, 55, -29, 42}
+	if !SlicesIntEqual(a, b) {
+		t.Errorf("expected slices equal to be true")
+	}
+	a = []int{123, 44, 55, -29, 42, 0}
+	b = []int{123, 44, 55, -29, 42}
+	if SlicesIntEqual(a, b) {
+		t.Errorf("expected slices equal to be false")
+	}
+	a = []int{123, 44, 55, -29, 41}
+	b = []int{123, 44, 55, -29, 42}
+	if SlicesIntEqual(a, b) {
+		t.Errorf("expected slices equal to be false")
+	}
+}
+
+
+func TestSliceStringToInt(t *testing.T) {
+	testSlice := []string{"123", "44", "55", "-29", "42"}
+	expected := []int{123, 44, 55, -29, 42}
+	if !SlicesIntEqual(SliceStringToInt(testSlice), expected) {
+		t.Errorf("converted slice did not match expected")
 	}
 }
