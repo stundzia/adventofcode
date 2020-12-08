0c6d06c83d38412e78253bc71ddd23821199e998
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-08 08:32:05 +0200

    Add Day 8 test

--- 2020/day8/day8_test.go
+++ 2020/day8/day8_test.go
@@ -4,6 +4,31 @@ import (
 	"testing"
 )
 
-func TestSomething(t *testing.T) {
-
+func TestParseInstruction(t *testing.T) {
+	tcs := []struct{
+		Case string
+		Op string
+		Arg int
+	}{
+		{
+			"acc +50",
+			"acc",
+			50,
+		},
+		{
+			"nop -21",
+			"nop",
+			-21,
+		},
+		{
+			"jmp +461",
+			"jmp",
+			461,
+		},
+	}
+	for _, tc := range tcs {
+		if op, arg := parseInstruction(tc.Case); op != tc.Op || arg != tc.Arg {
+			t.Errorf("case `%s` erroneusly got op, arg: %s %d", tc.Case, op, arg)
+		}
+	}
 }
