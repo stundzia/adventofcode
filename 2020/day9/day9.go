63e748f20899dcab4fe10361992ab1974adbb4ef
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-09 07:28:00 +0200

    Day 9: initial messsy commit

---
+++ 2020/day9/day9.go
@@ -0,0 +1,78 @@
+package day9
+
+import (
+	"fmt"
+	"github.com/stundzia/adventofcode/utils"
+	"strconv"
+)
+
+func isValid(nums []int, n int, preamble int) bool {
+	if n < preamble {
+		return true
+	}
+	num := nums[n]
+	components := nums[n - preamble:n]
+	for i, c := range components {
+		for i2, c2 := range components {
+			if c + c2 == num && i != i2 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func contiguousSet(nums []int, n int, invalidNum int) (found bool, set []int) {
+	for ;; n++ {
+		set = append(set, nums[n])
+		sum := utils.SumIntSlice(set)
+		if sum < invalidNum {
+			continue
+		} else {
+			if sum == invalidNum && len(set) > 1 {
+				fmt.Println(set)
+				fmt.Println(sum)
+				fmt.Println(invalidNum)
+				return true, set
+			} else {
+				return false, set
+			}
+		}
+	}
+}
+
+
+func DoSilver() string {
+	input, _ := utils.ReadInputFileContentsAsIntSlice(2020, 9, "\n")
+	for i, num := range input {
+		if !isValid(input, i, 25) {
+			return strconv.Itoa(num)
+		}
+	}
+	return strconv.Itoa(-1)
+}
+
+
+func DoGold() string {
+	invalidNum, _ := strconv.Atoi(DoSilver())
+	input, _ := utils.ReadInputFileContentsAsIntSlice(2020, 9, "\n")
+	set := []int{}
+	var found bool
+	for i, _ := range input {
+		if found, set = contiguousSet(input, i, invalidNum); found {
+			break
+		}
+	}
+	minSet := 9999999
+	maxSet := 0
+	for _, num := range set {
+		if num < minSet {
+			minSet = num
+		}
+		if num > maxSet {
+			maxSet = num
+		}
+	}
+
+	return strconv.Itoa(minSet + maxSet)
+}
---
+++ 2020/day9/day9_test.go
@@ -0,0 +1,34 @@
+package day9
+
+import (
+	"testing"
+)
+
+func TestParseInstruction(t *testing.T) {
+	tcs := []struct{
+		Case string
+		Op string
+		Arg int
+	}{
+		{
+			"acc +50",
+			"acc",
+			50,
+		},
+		{
+			"nop -21",
+			"nop",
+			-21,
+		},
+		{
+			"jmp +461",
+			"jmp",
+			461,
+		},
+	}
+	for _, tc := range tcs {
+		if op, arg := parseInstruction(tc.Case); op != tc.Op || arg != tc.Arg {
+			t.Errorf("case `%s` erroneusly got op, arg: %s %d", tc.Case, op, arg)
+		}
+	}
+}
