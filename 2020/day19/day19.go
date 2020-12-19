9240b43595b6ceb5d4f4bc04695040feedfda9fd
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-19 12:41:15 +0200

    Day 19 part 2: messy bs and I have no patience to refactor

--- 2020/day19/day19.go
+++ 2020/day19/day19.go
@@ -11,7 +11,7 @@ func DoSilver() string {
 	input, _ := utils.ReadInputFileContentsAsStringSlice(2020, 19, "\n\n")
 	rules := strings.Split(input[0], "\n")
 	msgs := strings.Split(input[1], "\n")
-	rm := NewRuleMatcher(rules)
+	rm := NewRuleMatcher(rules, 100)
 	validCount := 0
 	for _, msg := range msgs {
 		if rm.RuleMap[0].MessageValid(msg) {
@@ -23,5 +23,33 @@ func DoSilver() string {
 
 func DoGold() string {
 	input, _ := utils.ReadInputFileContentsAsStringSlice(2020, 19, "\n\n")
-	return fmt.Sprintf("%d", len(input))
+	rules := strings.Split(input[0], "\n")
+	msgs := strings.Split(input[1], "\n")
+
+	maxLen := 1
+	for _, msg := range msgs {
+		if len(msg) > maxLen {
+			maxLen = len(msg)
+		}
+	}
+	rm := NewRuleMatcher(rules, maxLen)
+	// Part 2 condition:
+	rm.RuleMap[8].RuleString = "8: 42 | 42 8"
+	rm.RuleMap[8].RuleString = "8: 42 | 42 42 | 42 42 42 | 42 42 42 42"
+	//rm.RuleMap[8].RuleMatch = [][]int{{42}, {42, 8}}
+	rm.RuleMap[8].RuleMatch = [][]int{{42}, {42, 42}, {42, 42, 42}, {42, 42, 42, 42}, {42, 42, 42, 42, 42}}
+	//rm.RuleMap[11].RuleString = "11: 42 31 | 42 11 31"
+	rm.RuleMap[11].RuleString = "11: 42 31 | 42 42 31 31"
+	//rm.RuleMap[11].RuleMatch = [][]int{{42, 31}, {42, 11, 31}}
+	rm.RuleMap[11].RuleMatch = [][]int{{42, 31}, {42, 42, 31, 31}}
+	// Basically the rule is "starts with rule 42, and ends with rule 31"
+
+
+	validCount := 0
+	for _, msg := range msgs {
+		if rm.MessageValidPart2v2(msg) {
+			validCount++
+		}
+	}
+	return fmt.Sprintf("%d", validCount)
 }
--- 2020/day19/models.go
+++ 2020/day19/models.go
@@ -9,6 +9,7 @@ import (
 
 type RuleMatcher struct {
 	RuleMap map[int]*Rule
+	MaxLen int
 }
 
 
@@ -18,6 +19,8 @@ type Rule struct {
 	RuleString string
 	StringMatch string
 	RuleMatch [][]int
+	MaxLen int
+	MinLen int
 }
 
 
@@ -25,7 +28,82 @@ func (r *Rule) MessageValid(msg string) bool {
 	if len(r.MatchingStrings) == 0 {
 		r.GetMatchingStrings()
 	}
-	return utils.StringSliceContains(r.MatchingStrings, msg)
+	valid := utils.StringSliceContains(r.MatchingStrings, msg)
+
+	return valid
+}
+
+func (rm *RuleMatcher) MessageValidPart2v2(msg string) bool {
+	lr := rm.RuleMap[42]
+	rr := rm.RuleMap[31]
+	lr.GetMinMaxLen()
+	rr.GetMinMaxLen()
+	// These are all the same
+	lrMin := lr.MinLen
+	lrMax := lr.MaxLen
+	rrMin := rr.MinLen
+	rrMax := rr.MaxLen
+
+	msgLen := len(msg)
+	valid := false
+
+	if msgLen % rrMax != 0 || msgLen < rrMin {
+		return false
+	}
+
+	lastMatchEnd := 0
+	Matched := 0
+
+	rightMatched := false
+	leftMatched := false
+	leftMatchedCount := 0
+	rightMatchedCount := 0
+
+
+	mainLoop:
+	for i := lrMin; i <= msgLen; i++ {
+		check := msg[lastMatchEnd:i]
+		checkLen := len(check)
+
+		if checkLen < rrMin && checkLen < lrMin {
+			continue
+		}
+
+		if !rightMatched {
+			if checkLen > lrMax {
+				break mainLoop
+			}
+			if lr.MessageValid(check) {
+				leftMatched = true
+				leftMatchedCount++
+				lastMatchEnd = i
+				Matched += checkLen
+			} else if rr.MessageValid(check) {
+				if !leftMatched {
+					break mainLoop
+				}
+				rightMatched = true
+				rightMatchedCount++
+				lastMatchEnd = i
+				Matched += checkLen
+			}
+		} else {
+			if checkLen > rrMax {
+				break mainLoop
+			}
+			if rr.MessageValid(check) {
+				lastMatchEnd = i
+				rightMatchedCount++
+				Matched += checkLen
+			}
+		}
+
+	}
+	if Matched == msgLen && leftMatched && rightMatched && leftMatchedCount >= 2 && leftMatchedCount > rightMatchedCount {
+		valid = true
+	}
+
+	return valid
 }
 
 
@@ -36,25 +114,72 @@ func (r *Rule) GetMatchingStrings() []string {
 	if r.StringMatch != "" {
 		return []string{r.StringMatch}
 	}
+	matchingStringsMap := map[string]struct{}{}
 	matchingStrings := []string{}
 	for _, rm := range r.RuleMatch {
-		if len(rm) == 3 {
+		if len(rm) == 5 {
+			matches1 := r.RM.RuleMap[rm[0]].GetMatchingStrings()
+			matches2 := r.RM.RuleMap[rm[1]].GetMatchingStrings()
+			matches3 := r.RM.RuleMap[rm[2]].GetMatchingStrings()
+			matches4 := r.RM.RuleMap[rm[3]].GetMatchingStrings()
+			matches5 := r.RM.RuleMap[rm[4]].GetMatchingStrings()
+			for _, m1 := range matches1 {
+				for _, m2 := range matches2 {
+					for _, m3 := range matches3 {
+						for _, m4 := range matches4 {
+							for _, m5 := range matches5 {
+								toAdd := m1 + m2 + m3 + m4 + m5
+								if len(toAdd) <= r.RM.MaxLen {
+									matchingStringsMap[toAdd] = struct{}{}
+								}
+							}
+						}
+					}
+				}
+			}
+		} else if len(rm) == 4 {
+			matches1 := r.RM.RuleMap[rm[0]].GetMatchingStrings()
+			matches2 := r.RM.RuleMap[rm[1]].GetMatchingStrings()
+			matches3 := r.RM.RuleMap[rm[2]].GetMatchingStrings()
+			matches4 := r.RM.RuleMap[rm[3]].GetMatchingStrings()
+			for _, m1 := range matches1 {
+				for _, m2 := range matches2 {
+					for _, m3 := range matches3 {
+						for _, m4 := range matches4 {
+							toAdd := m1 + m2 + m3 + m4
+							if len(toAdd) <= r.RM.MaxLen {
+								matchingStringsMap[toAdd] = struct{}{}
+							}
+						}
+					}
+				}
+			}
+		} else if len(rm) == 3 {
 			matches1 := r.RM.RuleMap[rm[0]].GetMatchingStrings()
 			matches2 := r.RM.RuleMap[rm[1]].GetMatchingStrings()
 			matches3 := r.RM.RuleMap[rm[2]].GetMatchingStrings()
 			for _, m1 := range matches1 {
 				for _, m2 := range matches2 {
 					for _, m3 := range matches3 {
-						matchingStrings = append(matchingStrings, m1 + m2 + m3)
+						toAdd := m1 + m2 + m3
+						if len(toAdd) <= r.RM.MaxLen {
+							matchingStringsMap[toAdd] = struct{}{}
+						}
 					}
 				}
 			}
 		} else if len(rm) == 2 {
 			matches1 := r.RM.RuleMap[rm[0]].GetMatchingStrings()
 			matches2 := r.RM.RuleMap[rm[1]].GetMatchingStrings()
+			if len(matches1) == 65792 || len(matches2) == 65792 {
+				fmt.Println("THIS")
+			}
 			for _, m1 := range matches1 {
 				for _, m2 := range matches2 {
-					matchingStrings = append(matchingStrings, m1 + m2)
+					toAdd := m1 + m2
+					if len(toAdd) <= r.RM.MaxLen {
+						matchingStringsMap[toAdd] = struct{}{}
+					}
 				}
 			}
 		} else if len(rm) == 1 {
@@ -64,19 +189,33 @@ func (r *Rule) GetMatchingStrings() []string {
 			panic("Not like this...")
 		}
 	}
+	for k, _ := range matchingStringsMap {
+		matchingStrings = append(matchingStrings, k)
+	}
 	r.MatchingStrings = matchingStrings
 	return matchingStrings
 }
 
+func (r *Rule) GetMinMaxLen() {
+	if len(r.MatchingStrings) == 0 {
+		r.GetMatchingStrings()
+	}
+	minL := 100
+	maxL := 0
+	for _, s := range r.MatchingStrings {
+		l := len(s)
+		if l < minL {
+			minL = l
+		}
+		if l > maxL {
+			maxL = l
+		}
+	}
+	r.MaxLen = maxL
+	r.MinLen = minL
+}
+
 func (rm *RuleMatcher) parseRule(rule string) {
-	//80: 131 97
-	//3: 123 107 | 97 66
-	//101: 1 123 | 95 97
-	//10: 97 138 | 123 107
-	//83: 123 27 | 97 12
-	//54: 97 126 | 123 107
-	//61: 97 50 | 123 114
-	//123: "a"
 	parts := strings.Split(rule, ": ")
 	ruleId, _ := strconv.Atoi(parts[0])
 	ruleStr := parts[1]
@@ -99,13 +238,10 @@ func (rm *RuleMatcher) parseRule(rule string) {
 		RuleMatch:       ruleMatch,
 	}
 	rm.RuleMap[ruleId] = ruleRef
-	fmt.Println(rm.RuleMap[ruleId].RuleString)
-	fmt.Println(rm.RuleMap[ruleId].StringMatch)
-	fmt.Println(rm.RuleMap[ruleId].RuleMatch)
 }
 
-func NewRuleMatcher(rules []string) *RuleMatcher {
-	rm := &RuleMatcher{RuleMap: map[int]*Rule{}}
+func NewRuleMatcher(rules []string, maxLen int) *RuleMatcher {
+	rm := &RuleMatcher{RuleMap: map[int]*Rule{}, MaxLen: maxLen}
 	for _, rule := range rules {
 		rm.parseRule(rule)
 	}
