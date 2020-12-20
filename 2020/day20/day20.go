0acfa9d791f314c86e431973b0cc1964445accb8
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-20 13:34:31 +0200

    Day 20 part 2: finally

--- 2020/day20/day20.go
+++ 2020/day20/day20.go
@@ -3,6 +3,8 @@ package day20
 import (
 	"fmt"
 	"github.com/stundzia/adventofcode/utils"
+	"math/rand"
+	"time"
 )
 
 
@@ -28,5 +30,64 @@ func DoSilver() string {
 
 func DoGold() string {
 	input, _ := utils.ReadInputFileContentsAsStringSlice(2020, 20, "\n\n")
-	return fmt.Sprintf("%d", len(input))
+	img := NewImageFromInput(input)
+	for _, tile := range img.Tiles {
+		tile.FindPotentialPairings()
+	}
+
+	cornerIds := []int{}
+	cornerTiles := []*Tile{}
+	for _, tile := range img.Tiles {
+		if len(tile.PairingMap) == 2 {
+			cornerIds = append(cornerIds, tile.Id)
+			cornerTiles = append(cornerTiles, tile)
+		}
+	}
+	res := 1
+	for _, cId := range cornerIds {
+		res *= cId
+	}
+
+	candidate := cornerTiles[2]
+	candidate.FindPotentialPairings()
+	img.PositionTile(candidate, [2]int{0,0}) // yup, cheated and found it by trial and error (properly aligned corner)
+	for i := 0; i < 100; i++ {
+		img.PositionAllTiles()
+	}
+
+
+	for _, tile := range img.Tiles {
+		tile.RemoveBorders()
+	}
+	img.FormFullImageMatrix(false)
+
+	rand.Seed(time.Now().Unix())
+
+	monsterCount, seaCount := 0, 0
+
+	for i := 0; i < 22222; i++ {
+		if rand.Int() % 4 == 0 {
+			img.RotateClockwise()
+		}
+		if rand.Int() % 3 == 0 {
+			if rand.Int() % 2 == 0 {
+				img.Flip(true)
+			} else {
+				img.Flip(false)
+			}
+		}
+
+		for y, line := range img.FullImageMatrix {
+			for x, pixel := range line {
+				if pixel == "#" {
+					img.MarkIfMonster([]int{y,x})
+				}
+			}
+		}
+		monsterCount, seaCount = img.GetMonsterAndSeaCount()
+		if monsterCount != 0 {
+			break
+		}
+	}
+	return fmt.Sprintf("%d (Monster count: %d)", seaCount, monsterCount)
 }
--- 2020/day20/models.go
+++ 2020/day20/models.go
@@ -3,6 +3,7 @@ package day20
 import (
 	"fmt"
 	"github.com/stundzia/adventofcode/utils"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -10,26 +11,90 @@ import (
 type Image struct {
 	Tiles []*Tile
 	Matrix [12][12]*Tile
+	FullImageMatrix [][]string
 }
 
 type Tile struct {
 	Img *Image
 	Id int
+	Coords [2]int
 	Matrix [][]string
 	Borders map[string]string
 	PairingSlice []int
 	PairingMap map[string]*Tile
+	Adjacent map[string]*Tile
+}
+
+var oppositeSides = map[string]string {
+	"top": "bot",
+	"bot": "top",
+	"left": "right",
+	"right": "left",
+}
+
+var monsterPositionsFromTail = [][2]int{
+	{0,0},
+	{1,1},
+	{1,4},
+	{0,5},
+	{0,6},
+	{1,7},
+	{1,10},
+	{0,11},
+	{0,12},
+	{1,13},
+	{1,16},
+	{0,17},
+	{0,18},
+	{-1,18},
+	{0,19},
 }
 
 func NewImageFromInput(input []string) *Image {
 	img := &Image{
 		Tiles: []*Tile{},
 		Matrix: [12][12]*Tile{},
+		FullImageMatrix: [][]string{},
 	}
 	for _, ts := range input {
 		img.NewTileFromStringSlice(strings.Split(ts, "\n"))
 	}
 	return img
+}
+
+
+func (img *Image) GetCoordsValue(coords []int) string {
+	sideLen := len(img.FullImageMatrix)
+	if coords[0] >= sideLen || coords[1] >= sideLen {
+		return "e"
+	}
+	return img.FullImageMatrix[coords[0]][coords[1]]
+}
+
+func (t *Tile) RemoveBorders() {
+	t.Matrix = t.Matrix[1:len(t.Matrix) - 1]
+	for i, line := range t.Matrix {
+		t.Matrix[i] = line[1:len(line) -1]
+	}
+}
+
+func (img *Image) MarkIfMonster(tailCoords []int) {
+	if img.PixelIsMonsterTail(tailCoords) {
+		for _, delta := range monsterPositionsFromTail {
+			coords := []int{tailCoords[0] + delta[0], tailCoords[1] + delta[1]}
+			img.FullImageMatrix[coords[0]][coords[1]] = "O"
+		}
+	}
+}
+
+func (img *Image) PixelIsMonsterTail(tailCoords []int) bool {
+	for _, delta := range monsterPositionsFromTail {
+		coords := []int{tailCoords[0] + delta[0], tailCoords[1] + delta[1]}
+		if img.GetCoordsValue(coords) != "#" {
+			return false
+		}
+	}
+	return true
 }
 
 
@@ -43,11 +108,75 @@ func (img *Image) NewTileFromStringSlice(ss []string) *Tile {
 
 	tile := &Tile{
 		Img: img,
+		Coords: [2]int{-1, -1},
 		Id:      id,
 		Matrix:  matrix,
+		Adjacent: map[string]*Tile{},
 	}
 	img.Tiles = append(img.Tiles, tile)
 	return tile
+}
+
+func (img *Image) PositionTile(tile *Tile, position [2]int) {
+	img.Matrix[position[0]][position[1]] = tile
+	tile.Coords = position
+}
+
+func (img *Image) PositionAllTiles() {
+	for _, line := range img.Matrix {
+		for _, tile := range line {
+			if tile != nil {
+				tile.PositionPairings()
+			}
+		}
+	}
+}
+
+func (t *Tile) PositionTileAtSide(tile *Tile,side string) {
+	switch side {
+	case "left":
+		t.Img.PositionTile(tile, [2]int{t.Coords[0], t.Coords[1] - 1})
+	case "right":
+		t.Img.PositionTile(tile, [2]int{t.Coords[0], t.Coords[1] + 1})
+	case "top":
+		t.Img.PositionTile(tile, [2]int{t.Coords[0] - 1, t.Coords[1]})
+	case "bot":
+		t.Img.PositionTile(tile, [2]int{t.Coords[0] + 1, t.Coords[1]})
+	default:
+		panic("aaaahhhh, wtf?!")
+	}
+	tile.FindPotentialPairings()
+}
+
+
+func (t *Tile) PositionPairings() {
+	for side, otherTile := range t.PairingMap {
+		if t.Adjacent[side] != nil {
+			continue
+		}
+		aligned := false
+		for i := 0;aligned == false;i++ {
+			if t.Borders[side] == otherTile.Borders[oppositeSides[side]] {
+				aligned = true
+				t.PositionTileAtSide(otherTile, side)
+				t.Adjacent[side] = otherTile
+				otherTile.Adjacent[oppositeSides[side]] = t
+				break
+			} else if (side == "left" || side == "right") && t.Borders[side] == utils.ReverseString(otherTile.Borders[oppositeSides[side]]) {
+				otherTile.Flip(true)
+				otherTile.GetBorders(true)
+			} else if (side == "top" || side == "bot") && t.Borders[side] == utils.ReverseString(otherTile.Borders[oppositeSides[side]]) {
+				otherTile.Flip(false)
+				otherTile.GetBorders(true)
+			} else {
+				otherTile.RotateClockwise()
+				otherTile.GetBorders(true)
+			}
+			if i > 10 {
+				panic("failed")
+			}
+		}
+	}
 }
 
 func (t *Tile) PrintMatrix() {
@@ -81,11 +210,12 @@ func (t *Tile) GetBorders(forceRecalc bool) map[string]string {
 func (t *Tile) Flip(vertically bool) {
 	if vertically {
 		t.Matrix = utils.ReverseStringSliceSlice(t.Matrix)
-		return
+	} else {
+		for i, line := range t.Matrix {
+			t.Matrix[i] = utils.ReverseStringSlice(line)
+		}
 	}
-	for i, line := range t.Matrix {
-		t.Matrix[i] = utils.ReverseStringSlice(line)
-	}
+	t.GetBorders(true)
 }
 
 func (t *Tile) RotateClockwise() {
@@ -93,15 +223,16 @@ func (t *Tile) RotateClockwise() {
 	for i := len(t.Matrix) - 1; i >= 0; i-- {
 		cols = append(cols, t.Matrix[i])
 	}
-	newMatrx := [][]string{}
+	newMatrix := [][]string{}
 	for i := 0; i < len(t.Matrix); i++ {
 		line := []string{}
 		for _, col := range cols {
 			line = append(line, col[i])
 		}
-		newMatrx = append(newMatrx, line)
+		newMatrix = append(newMatrix, line)
 	}
-	t.Matrix = newMatrx
+	t.Matrix = newMatrix
+	t.GetBorders(true)
 }
 
 
@@ -121,4 +252,81 @@ func (t *Tile) FindPotentialPairings() {
 		}
 	}
 	t.PairingMap = pairingMap
+}
+
+func (img *Image) FormFullImageMatrix(printIt bool) {
+	lines := []string{}
+	for _, tileLine := range img.Matrix {
+		for y := 0; y < len(tileLine[0].Matrix); y++ {
+			l := ""
+			for _, tile := range tileLine {
+				l += strings.Join(tile.Matrix[y], "")
+			}
+			lines = append(lines, l)
+		}
+	}
+	for _, line := range lines {
+		img.FullImageMatrix = append(img.FullImageMatrix, strings.Split(line, ""))
+	}
+	if printIt {
+		for _, l := range lines {
+			fmt.Println(l)
+		}
+	}
+}
+
+
+func (img *Image) Flip(vertically bool) {
+	if vertically {
+		img.FullImageMatrix = utils.ReverseStringSliceSlice(img.FullImageMatrix)
+		return
+	}
+	for i, line := range img.FullImageMatrix {
+		img.FullImageMatrix[i] = utils.ReverseStringSlice(line)
+	}
+}
+
+
+func (img *Image) RotateClockwise() {
+	cols := [][]string{}
+	for i := len(img.FullImageMatrix) - 1; i >= 0; i-- {
+		cols = append(cols, img.FullImageMatrix[i])
+	}
+	newMatrix := [][]string{}
+	for i := 0; i < len(img.FullImageMatrix); i++ {
+		line := []string{}
+		for _, col := range cols {
+			line = append(line, col[i])
+		}
+		newMatrix = append(newMatrix, line)
+	}
+	img.FullImageMatrix = newMatrix
+}
+
+func (img *Image) GetMonsterAndSeaCount() (monsterCount, seaCount int) {
+	pixelCount := 0
+	pixelCount2 := 0
+	for _, line := range img.FullImageMatrix {
+		pixelCount2 += len(line)
+		for _, pixel := range line {
+			pixelCount++
+			if pixel == "#" {
+				seaCount++
+			}
+			if pixel == "O" {
+				monsterCount++
+			}
+		}
+	}
+	return
+}
+
+func (img *Image) DumpToFile(fName string) {
+	f, err := os.Create(fName)
+	if err != nil {
+		panic("well, fuck")
+	}
+	for _, line := range img.FullImageMatrix {
+		_, _ = f.WriteString(fmt.Sprintf("%s\n", strings.Join(line, "")))
+	}
 }
