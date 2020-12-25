7bf386e05cc02a217cd9f65c5c26e12c2b17884b
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-25 08:20:25 +0200

    Day 25 part 1: initial

---
+++ 2020/day25/day25.go
@@ -0,0 +1,44 @@
+package day25
+
+import (
+	"fmt"
+	"github.com/stundzia/adventofcode/utils"
+)
+
+const SubjectDivider = 20201227
+const SubjectNumber = 7
+
+
+func getLoopSize(pKey int) int {
+	loopSize := 0
+	val := 1
+	for ; val != pKey; {
+		val *= SubjectNumber
+		val %= SubjectDivider
+		loopSize++
+	}
+	return loopSize
+}
+
+func transform(pKey, loopSize int) int {
+	val := 1
+	for i := 0; i < loopSize; i++ {
+		val *= pKey
+		val %= SubjectDivider
+	}
+	return val
+}
+
+func DoSilver() string {
+	input, _ := utils.ReadInputFileContentsAsIntSlice(2020, 25, "\n")
+
+	cardLoopSize := getLoopSize(input[0])
+	doorLoopSize := getLoopSize(input[1])
+
+	return fmt.Sprintf("%d (should be equal to %d)", transform(input[0], doorLoopSize), transform(input[1], cardLoopSize))
+}
+
+func DoGold() string {
+	input, _ := utils.ReadInputFileContentsAsStringSlice(2020, 25, "\n")
+	return fmt.Sprintf("%d", len(input))
+}
