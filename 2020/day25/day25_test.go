6225835f7598361fd6dd20a123be8c2a3bfa610d
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-28 10:18:34 +0200

    Day 25 tests

--- 2020/day25/day25_test.go
+++ 2020/day25/day25_test.go
@@ -1,27 +1,62 @@
 package day25
 
 import (
-	"github.com/stundzia/adventofcode/utils"
 	"testing"
 )
 
-func TestSample(t *testing.T) {
+func TestGetLoopSize(t *testing.T) {
 	tcs := []struct{
 		test string
-		slice []int
+		key int
 		expected int
 	}{
 		{
-			"Slice: 1,3,2 expected sum: 6",
-			[]int{1,3,2},
-			6,
+			"Pkey: 5764801, expected loopsize 8",
+			5764801,
+			8,
+		},
+		{
+			"Pkey: 17807724, expected loopsize 11",
+			17807724,
+			11,
 		},
 	}
 	for _, tc := range tcs {
 		t.Run(tc.test, func(t *testing.T) {
-			sum := utils.SumIntSlice(tc.slice)
-			if sum != tc.expected {
-				t.Errorf("expected %d, but got %d", tc.expected, sum)
+			loopSize := getLoopSize(tc.key)
+			if loopSize != tc.expected {
+				t.Errorf("expected %d, but got %d", tc.expected, loopSize)
+			}
+		})
+	}
+}
+
+
+func TestTransform(t *testing.T) {
+	tcs := []struct{
+		test string
+		key int
+		loopSize int
+		expected int
+	}{
+		{
+			"Pkey: 17807724, loopsize 8, expected: 14897079",
+			17807724,
+			8,
+			14897079,
+		},
+		{
+			"Pkey: 5764801, loopsize 11, expected: 14897079",
+			5764801,
+			11,
+			14897079,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.test, func(t *testing.T) {
+			encryptionKey := transform(tc.key, tc.loopSize)
+			if encryptionKey != tc.expected {
+				t.Errorf("expected %d, but got %d", tc.expected, encryptionKey)
 			}
 		})
 	}
