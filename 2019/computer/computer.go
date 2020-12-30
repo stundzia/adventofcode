e0c8e54e4088960c18da8e160945da04af01e34a
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-30 10:43:30 +0200

    Update 2019 intcode computer with input/output opcode support

--- 2019/computer/computer.go
+++ 2019/computer/computer.go
@@ -19,8 +19,8 @@ func NewComputer(opcodes []int) *Computer {
 	}
 	return &Computer{
 		Opcodes:    opArray,
-		InputPipe:  make(chan int),
-		OutputPipe: make(chan int),
+		InputPipe:  make(chan int, 1),
+		OutputPipe: make(chan int, 1),
 	}
 }
 
@@ -31,23 +31,39 @@ func (c *Computer) Run() (int, error) {
 			if position >= len(c.Opcodes) {
 				return 0, NewPositionOutOfRangeError(fmt.Sprintf("position %d is out of range for opcodes with len %d", position, len(c.Opcodes)))
 			}
-			op := c.Opcodes[position]
-			switch op {
+
+			opCode := c.Opcodes[position]
+			switch opCode {
+
 			case 1:
 				op1 := c.Opcodes[c.Opcodes[position + 1]]
 				op2 := c.Opcodes[c.Opcodes[position + 2]]
-				c.Opcodes[c.Opcodes[position + 3]] = op1 + op2
+				address := c.Opcodes[position + 3]
+				c.Opcodes[address] = op1 + op2
 				position += 4
 
 			case 2:
 				op1 := c.Opcodes[c.Opcodes[position + 1]]
 				op2 := c.Opcodes[c.Opcodes[position + 2]]
-				c.Opcodes[c.Opcodes[position + 3]] = op1 * op2
+				address := c.Opcodes[position + 3]
+				c.Opcodes[address] = op1 * op2
 				position += 4
+
+			case 3:
+				address := c.Opcodes[position + 1]
+				c.Opcodes[address] = <- c.InputPipe
+				position += 2
+
+			case 4:
+				address := c.Opcodes[position + 1]
+				c.OutputPipe <- c.Opcodes[address]
+				position += 2
+
 			case 99:
 				break main
+
 			default:
-				return 0, NewUnknownOpcodeError(fmt.Sprintf("unknown opcode received: %d", op))
+				return 0, NewUnknownOpcodeError(fmt.Sprintf("unknown opcode received: %d", opCode))
 			}
 		}
 	return c.Opcodes[0], nil
--- 2019/computer/computer_test.go
+++ 2019/computer/computer_test.go
@@ -15,3 +15,37 @@ func TestComputerBasicRun(t *testing.T) {
 		t.Errorf("incorrect opcode at position 0, expected 3500, but found %d", res)
 	}
 }
+
+func TestComputerWithInputOutput(t *testing.T) {
+	opcodes := []int{
+		1,19,20,3,
+		2,3,21,0,
+		3,128,
+		4,128,
+		3,256,
+		4,256,
+		4,0,
+		99,
+		30,40,50,
+	}
+	comp := NewComputer(opcodes)
+	comp.InputPipe <- 25
+	go comp.Run()
+	output := <- comp.OutputPipe
+
+	if output != 25 {
+		t.Errorf("incorrect output, expected 25, but got %d", output)
+	}
+
+	comp.InputPipe <- 42
+	output = <- comp.OutputPipe
+
+	if output != 42 {
+		t.Errorf("incorrect output, expected 42, but got %d", output)
+	}
+
+	res := <- comp.OutputPipe
+	if res != 3500 {
+		t.Errorf("incorrect result (address 0), expected 3500, but got %d", res)
+	}
+}
