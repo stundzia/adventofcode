1f1ba5740868ac2451bfac3851e54d2abda7af89
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-30 09:33:00 +0200

    2019 day 4 both parts with tests

---
+++ 2019/day4/day4.go
@@ -0,0 +1,65 @@
+package day4
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isValid(pass int) bool {
+	passStr := strconv.Itoa(pass)
+	adjecentFound := false
+	var last int32
+	for _, char := range passStr {
+		if char < last {
+			return false
+		}
+		if char == last {
+			adjecentFound = true
+		}
+		last = char
+	}
+	return adjecentFound
+}
+
+func isValidV2(pass int) bool {
+	passStr := strconv.Itoa(pass)
+	adjecentCount := 0
+	adjecentFound := false
+	var last int32
+	for _, char := range passStr {
+		if char < last {
+			return false
+		}
+		if char == last {
+			adjecentCount++
+		} else {
+			if adjecentCount == 1 {
+				adjecentFound = true
+			}
+			adjecentCount = 0
+		}
+		last = char
+	}
+	return adjecentFound || adjecentCount == 1
+}
+
+func getPasswordCountInRange(min, max, version int) int {
+	count := 0
+	for i := min; i <= max; i++ {
+		if version == 1 && isValid(i) {
+			count++
+		} else if version == 2 && isValidV2(i) {
+			count++
+		}
+	}
+	return count
+}
+
+
+func DoSilver() string {
+	return fmt.Sprintf("Solution: %d", getPasswordCountInRange(197487, 673251, 1))
+}
+
+func DoGold() string {
+	return fmt.Sprintf("Solution: %d", getPasswordCountInRange(197487, 673251, 2))
+}
---
+++ 2019/day4/day4_test.go
@@ -0,0 +1,69 @@
+package day4
+
+import (
+	"testing"
+)
+
+func TestIsValid(t *testing.T) {
+	tcs := []struct{
+		test string
+		pass int
+		expected bool
+	}{
+		{
+			"pass: 111111, valid: yes",
+			111111,
+			true,
+		},
+		{
+			"pass: 223450, valid: no",
+			223450,
+			false,
+		},
+		{
+			"pass: 123789, valid: no",
+			123789,
+			false,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.test, func(t *testing.T) {
+			valid := isValid(tc.pass)
+			if valid != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, valid)
+			}
+		})
+	}
+}
+
+func TestIsValidV2(t *testing.T) {
+	tcs := []struct{
+		test string
+		pass int
+		expected bool
+	}{
+		{
+			"pass: 112233, valid: yes",
+			112233,
+			true,
+		},
+		{
+			"pass: 123444, valid: no",
+			123444,
+			false,
+		},
+		{
+			"pass: 111122, valid: yes",
+			111122,
+			true,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.test, func(t *testing.T) {
+			valid := isValidV2(tc.pass)
+			if valid != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, valid)
+			}
+		})
+	}
+}
