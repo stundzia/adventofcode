ed262e6488adba6b27958cf4332fd6468c7159e9
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-12-30 14:55:41 +0200

    2019 day 5 part 2

--- 2019/day5/day5.go
+++ 2019/day5/day5.go
@@ -19,10 +19,20 @@ func DoSilver() string {
 			break
 		}
 	}
-	return fmt.Sprintf("Solution: %d", lastOutput)
+	return fmt.Sprintf("%d", lastOutput)
 }
 
 func DoGold() string {
-	input, _ := utils.ReadInputFileContentsAsIntSlice(2019, 4, "\n")
-	return fmt.Sprintf("Solution: %d", len(input))
+	opcodes, _ := utils.ReadInputFileContentsAsIntSlice(2019, 5, ",")
+	comp := computer.NewComputer(opcodes)
+	go comp.Run()
+	comp.InputPipe <- 5
+	var lastOutput int
+	for ;; {
+		lastOutput = <- comp.OutputPipe
+		if !comp.Running {
+			break
+		}
+	}
+	return fmt.Sprintf("%d", lastOutput)
 }
