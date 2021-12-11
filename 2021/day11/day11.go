654dbad190947955ec1008b9b5483ab097159313
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2021-12-11 07:29:04 +0200

    Add day 11

---
+++ 2021/day11/day11.go
@@ -0,0 +1,135 @@
+package day11
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/stundzia/adventofcode/utils"
+)
+
+type octoGrid struct {
+	flashCount int
+	octopi map[string]*octopus
+}
+
+type octopus struct {
+	octoGrid *octoGrid
+	x int
+	y int
+	energy int
+	adjacent map[string]*octopus
+	flashed bool
+}
+
+func (og *octoGrid) assignNeighbours() {
+	for coords, octo := range og.octopi {
+		coordsSlice := strings.Split(coords, ",")
+		x, _ := strconv.Atoi(coordsSlice[0])
+		y, _ := strconv.Atoi(coordsSlice[1])
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x+1, y)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x+1,y)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x+1, y+1)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x+1,y+1)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x+1, y-1)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x+1,y-1)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x-1, y)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x-1,y)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x-1, y+1)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x-1,y+1)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x-1, y-1)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x-1,y-1)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x, y+1)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x,y+1)] = nei
+		}
+		if nei, exists := og.octopi[fmt.Sprintf("%d,%d", x, y-1)]; exists {
+			octo.adjacent[fmt.Sprintf("%d,%d", x,y-1)] = nei
+		}
+	}
+}
+
+func (og *octoGrid) doStep() bool {
+	for _, octo := range og.octopi {
+		octo.flashed = false
+		octo.energy++
+	}
+	for _, octo := range og.octopi {
+		octo.flashIfNeeded()
+	}
+	for _, octo := range og.octopi {
+		if octo.flashed {
+			octo.energy = 0
+		}
+	}
+	for _, octo := range og.octopi {
+		if octo.flashed {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func (o *octopus) flashIfNeeded() bool {
+	if o.energy > 9 && o.flashed == false {
+		o.flashed = true
+		o.octoGrid.flashCount++
+		for _, nei := range o.adjacent {
+			nei.energy += 1
+			nei.flashIfNeeded()
+		}
+		return true
+	}
+	return false
+}
+
+func newOctoGrid(nums []string) *octoGrid {
+	og := &octoGrid{
+		flashCount: 0,
+		octopi:     map[string]*octopus{},
+	}
+	for y, line := range nums {
+		valsStr := strings.Split(line, "")
+		for x, val := range valsStr {
+			valInt, _ := strconv.Atoi(val)
+			og.octopi[fmt.Sprintf("%d,%d", x,y)] = &octopus{
+				octoGrid: og,
+				x:        x,
+				y:        y,
+				energy: valInt,
+				adjacent: map[string]*octopus{},
+			}
+		}
+	}
+	og.assignNeighbours()
+	return og
+}
+
+func DoSilver() string {
+	nums, _ := utils.ReadInputFileContentsAsStringSlice(2021, 11, "\n")
+	og := newOctoGrid(nums)
+	for i := 0; i < 100; i++ {
+		og.doStep()
+	}
+	return fmt.Sprintf("Solution: %d",og.flashCount)
+}
+
+func DoGold() string {
+	nums, _ := utils.ReadInputFileContentsAsStringSlice(2021, 11, "\n")
+	og := newOctoGrid(nums)
+	res := 0
+	for i := 0; ; i++ {
+		if og.doStep() {
+			res = i + 1
+			break
+		}
+	}
+	return fmt.Sprintf("Solution: %d", res)
+}
