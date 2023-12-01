2df3b43bd81b2f55f2da069976704b56373023bf
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2023-12-01 07:27:56 +0200

    2023 day 1 both parts

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -2,24 +2,90 @@ package day1
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/stundzia/adventofcode/utils"
 )
 
-func DoSilver() string {
-	//input, _ := utils.ReadInputFileContentsAsIntSliceLines(2023, 1)
-	//input, _ := utils.ReadInputFileContentsAsIntSlice(2023, 1)
-	//input, _ := utils.ReadInputFileContentsAsStringSlice(2023, 1)
+var digitMap = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
 
+func DoSilver() string {
+	input, _ := utils.ReadInputFileContentsAsStringSlice(2023, 1, "\n")
 	res := 0
+	for _, l := range input {
+		first := -1
+		last := -1
+		for _, c := range l {
+			d, err := strconv.Atoi(string(c))
+			if err != nil {
+				continue
+			}
+			if first == -1 {
+				first = d
+			}
+			last = d
+		}
+		add, err := strconv.Atoi(fmt.Sprintf("%d%d", first, last))
+		if err != nil {
+			log.Fatal(err)
+		}
+		res += add
+	}
 
 	return fmt.Sprintf("Solution: %d", res)
 }
 
-func DoGold() string {
-	//input, _ := utils.ReadInputFileContentsAsIntSliceLines(2023, 1)
-	//input, _ := utils.ReadInputFileContentsAsIntSlice(2023, 1)
-	//input, _ := utils.ReadInputFileContentsAsStringSlice(2023, 1)
+func handleLineP2(line string) int {
+	first := -1
+	last := -1
+	from := 0
+	for i, c := range line {
+		d, err := strconv.Atoi(string(c))
+		if err == nil {
+			if first == -1 {
+				first = d
+			}
+			last = d
+			from = i
+			continue
+		}
+		for k, v := range digitMap {
+			if strings.Contains(line[from:i+1], k) {
+				if first == -1 {
+					first = v
+				}
+				last = v
+				from = i
+				continue
+			}
+		}
+	}
+	add, err := strconv.Atoi(fmt.Sprintf("%d%d", first, last))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	return add
+}
+
+func DoGold() string {
+	input, _ := utils.ReadInputFileContentsAsStringSlice(2023, 1, "\n")
 	res := 0
+	for _, l := range input {
+		res += handleLineP2(l)
+	}
 
 	return fmt.Sprintf("Solution: %d", res)
 }
